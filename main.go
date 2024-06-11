bd3bac9b41f2f079bae0b86e8bb867224abb4289
mahi <mahidhardevisetty@gmail.com>
2024-06-11 16:32:39 +0530

    completed gin and test and benchmarking

--- main.go
+++ main.go
@@ -2,50 +2,59 @@ package main
 
 import (
 	"app/multi_cache"
-	"fmt"
-	"sync"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+// Capacity of cache
 const length = 3
 
 func main() {
+	// Importing the multi_cache package
 	cache := multi_cache.NewMultiCache()
-	var wg sync.WaitGroup
 
-	fmt.Println("Setting key1, key2, key3")
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		cache.Set("key1", "value1", length)
-		cache.Set("key2", "value2", length)
-		cache.Set("key3", "value3", length)
-	}()
-	wg.Wait()
-	cache.Print()
+	//storing the values in the cache
+	cache.Set("a", "1", length)
+	cache.Set("b", "2", length)
 
-	fmt.Println("Getting key2")
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		cache.Get("key2")
-	}()
-	wg.Wait()
-	cache.Print()
+	//intiliazes the restapi with gin
+	r := gin.Default()
 
-	fmt.Println("Setting key3 to new value4")
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		cache.Set("key3", "value4", length)
-	}()
-	wg.Wait()
-	cache.Print()
+	// Endpoint to retrieve a value by key
+	r.GET("/:key", func(ctx *gin.Context) {
+		k := ctx.Param("key")
+		ctx.JSON(http.StatusOK, cache.Get(k))
+	})
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		cache.Del("key3")
-	}()
-	wg.Wait()
-	cache.Print()
+	// Endpoint to delete a value by key
+	r.DELETE("/:key", func(ctx *gin.Context) {
+		//storing the key value
+		k := ctx.Param("key")
+		//calling the delete method
+		cache.Del(k)
+	})
+
+	// Endpoint to set a key-value pair
+	r.POST("/:key/:value", func(ctx *gin.Context) {
+		//storing the key and value pair
+		k := ctx.Param("key")
+		v := ctx.Param("value")
+		//calling the set methods and sending the key,value and length
+		cache.Set(k, v, length)
+	})
+
+	// Endpoint to print the redis cache contents
+	r.GET("/redis/print", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, cache.Print_redis())
+	})
+
+	// Endpoint to print the in-memory cache contents
+	r.GET("/inmemory/print", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, cache.Print_in_mem())
+	})
+
+	// Start the Gin server
+	r.Run()
+
 }
